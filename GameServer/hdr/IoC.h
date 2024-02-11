43b834f7722537e2b37613f03b65c1df71745ead
@s.kriuchkov <s.kriuchkov@stc-spb.ru>
2024-02-11 19:52:43 +0300

    [+] Variadic in IoC

--- GameServer/hdr/IoC.h
+++ GameServer/hdr/IoC.h
@@ -21,36 +21,41 @@ public:
     template<class T, class ...Args>
     static std::shared_ptr<T> Resolve(const std::string& dependency, Args... args)
     {
-        auto factory = factoryMap<T>.find(dependency);
-        if(factory == factoryMap<T>.end())
+        auto factory = factoryMap<T, Args...>.find(dependency);
+        if(factory == factoryMap<T, Args...>.end())
             throw ArgumentException(dependency);
 
         return std::any_cast<Generator<T, Args...>>(factory->second)(args...);
     }
 
     using Map = std::map<std::string, std::any>;
+    using PMap = std::shared_ptr<Map>;
 
-    template<class T>
+    template<class T, class ...Args>
     static Map factoryMap;
 };
 
+
+
 template<class T, class ...Args>
-class Register : public ICommand
+class Register : public ICommand // Команда для регистрации лямбды, создающей необходимый тип
 {
 public:
     Register(std::string str, IoC::Generator<T, Args...> func) : str(str), func(func) {}
     virtual ~Register() {}
     virtual void Execute() override {
-        IoC::factoryMap<T>[str] = func;
+        IoC::factoryMap<T, Args...>[str] = func;
     }
 protected:
     std::string str;
     IoC::Generator<T, Args...> func;
 };
 
+
+
 template<class T, class ...Args>
-class InitCmd : public ICommand
-{
+class InitCmd : public ICommand // Команда для инициализации начанльный зависимостей (команды Register, создание скоупа и т.д.).
+{                               // Должна исполниться один раз для нового типа
 public:
     InitCmd() {}
     virtual ~InitCmd() {}
@@ -61,13 +66,13 @@ public:
         static IoC::Generator<ICommand, std::string, IoC::Generator<T, Args...>> gen = [](std::string str, IoC::Generator<T, Args...> func) {
             return std::make_shared<Register<T, Args...>>(str, func);
         };
-        IoC::factoryMap<ICommand>["Register"] = gen;
+        IoC::factoryMap<ICommand, std::string, IoC::Generator<T, Args...>>["IoC.Register"] = gen;
 
 
-        // static IoC::Generator<std::map<std::string, std::any>> emptyScope = [](){
-        //     return std::make_shared<std::map<std::string, std::any>>();
-        // };
-        // IoC::factoryMap<T>["IoC.Scope.Create.Empty"] = emptyScope;
+        static IoC::Generator<IoC::Map> createScope = [](){ // TODO: как регистрировать только один раз? куда положить?
+            return std::make_shared<IoC::Map>();
+        };
+        IoC::factoryMap<IoC::Map>["IoC.Scope.New"] = createScope;
 
         isInitialized = true;
     }
@@ -78,7 +83,7 @@ protected:
 template<class T, class ...Args>
 bool InitCmd<T, Args...>::isInitialized = false;
 
-template<class T>
+template<class T, class ...Args>
 IoC::Map IoC::factoryMap = IoC::Map();
 
 } // namespace Server
--- Tests/iocTest.h
+++ Tests/iocTest.h
@@ -6,18 +6,45 @@
 TEST(IOC, THROW_EXCEPTION)
 {
     Server::IoC ioc;
-    EXPECT_ANY_THROW(ioc.Resolve<int>("int"));
+    EXPECT_THROW(ioc.Resolve<int>("int"), Server::ArgumentException);
+
+    auto foo = [ioc](){ // отдельная функция, чтобы макрос сработал
+        ioc.Resolve<Server::ICommand, std::string, Server::IoC::Generator<int>>("IoC.Register", "int", []() {
+            return std::make_shared<int>(5);
+        })->Execute();
+    };
+
+    EXPECT_THROW(foo(), Server::ArgumentException);
 }
 
 TEST(IOC, REGISTER_DEPENDENCY)
 {
-    Server::InitCmd<int> cmd;
-    cmd.Execute();
+    Server::InitCmd<int> initInt;
+    Server::InitCmd<double> initDouble;
+    initInt.Execute();
+    initDouble.Execute();
 
     Server::IoC ioc;
-    ioc.Resolve<Server::ICommand, std::string, Server::IoC::Generator<int>>("Register", "int", []() -> std::shared_ptr<int> {
+    ioc.Resolve<Server::ICommand, std::string, Server::IoC::Generator<int>>("IoC.Register", "int", []() {
         return std::make_shared<int>(5);
     })->Execute();
 
+    ioc.Resolve<Server::ICommand, std::string, Server::IoC::Generator<double>>("IoC.Register", "double", []() {
+        return std::make_shared<double>(7.0);
+    })->Execute();
+
     EXPECT_EQ(*ioc.Resolve<int>("int"), 5);
+    EXPECT_DOUBLE_EQ(*ioc.Resolve<double>("double"), 7.0);
+}
+
+TEST(IOC, CREATE_SCOPE)
+{
+    Server::InitCmd<int> initInt;
+    initInt.Execute();
+
+    Server::IoC ioc;
+    auto ret = ioc.Resolve<Server::IoC::Map>("IoC.Scope.New");
+    
+    EXPECT_EQ(typeid(ret), typeid(Server::IoC::PMap));
+    EXPECT_TRUE(ret->empty());
 }
